4b87ee67c9ec5a7bf892b0fc454ba4aec2afe53a
Andri <andri.its@outlook.com>
2019-09-16 18:11:35 +0800

    Update run_DEVDAN.m

--- run_DEVDAN.m
+++ run_DEVDAN.m
@@ -3,9 +3,17 @@ clear
 close all
 
 
+% All the datasets used in our experiments can be downloaded in this link: https://bit.ly/2mhtRsE
 %% load data
-% load hyperplane; I = 4;
-load sea; I = 3;
+% load rotatedmnist; 	I = 784;
+% load permutedmnist; 	I = 784;
+% load mnist; 		    I = 784;
+% load forestcovtype; 	I = 54;
+% load sea; 		    I = 3;
+% load hyperplane;  	I = 4;
+% load occupancy; 	    I = 7;
+% load kddcup; 		    I = 41;
+% load Hepemass; 	    I = 27;
 
 %% This is ONLY for the numerical example where number of labeled data is limited, section 4.9
 selectiveSample.mode  = 0;       % 0: selective sample off, 1: selective sample on
